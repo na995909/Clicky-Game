6bff770043fab61d621ae6fbccea6d948cd4ff9d
Nelly Ackerman <pavel.ack@gmail.com>
2018-06-22 01:16:42 -0400

    week19HW

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// Footer component
+const Footer = props => (
+    <div class="footer-copyright py-3 text-center text-white">
+        Clicky Game!
+    </div>
+);
+
+export default Footer
